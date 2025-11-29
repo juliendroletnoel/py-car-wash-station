6856194ef8729df4bb7dc4b41ae05133aa0107ba
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-11-29 10:00:45 -0500

    Removed whitespace in blank lines

--- app/main.py
+++ app/main.py
@@ -12,28 +12,27 @@ class CarWashStation (object):
         self.clean_power = clean_power
         self.avergage_rating = average_rating
         self.count_of_ratings = count_of_ratings
-        
+
     def serve_cars(self, cars: list) -> float:
         total_price = 0
-        
+
         for car in cars:
             if car.clean_mark < self.clean_power:
                 total_price += self.calculating_washing_price(car)
                 self.wash_single_car(car)
-                
-        return total_price
     
+        return total_price
+
     def calculating_washing_price(self, car: Car) -> float:
-        price = car.comfort_class * \
-        (self.clean_power - car.clean_mark) * \
-        (self.avergage_rating / self.distance_from_city_center) 
+        price = car.comfort_class \
+        * (self.clean_power - car.clean_mark) \
+        * (self.avergage_rating / self.distance_from_city_center) 
 
         return round(price, 1)
-    
+
     def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
-        
+
     def rate_service(self, rate: float) -> None:
         self.avergage_rating = (self.avergage_rating * self.count_of_ratings + rate) / (self.count_of_ratings + 1)
         self.count_of_ratings += 1
-
