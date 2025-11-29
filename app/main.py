6f064af3ee14758ad02efc152e5641f8e4cd2dd1
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-11-29 10:05:31 -0500

    Removed blank line with extra 80 characters

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 class Car (object):
     # write your code here
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int, clean_mark: int, 
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -8,7 +9,8 @@ class Car (object):
 
 class CarWashStation (object):
     # write your code here
-    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int) -> None:
+    def __init__(self, distance_from_city_center: float, clean_power: int, 
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.avergage_rating = average_rating
@@ -35,5 +37,6 @@ class CarWashStation (object):
         car.clean_mark = self.clean_power
 
     def rate_service(self, rate: float) -> None:
-        self.avergage_rating = (self.avergage_rating * self.count_of_ratings + rate) / (self.count_of_ratings + 1)
+        self.avergage_rating = (self.avergage_rating \
+            * self.count_of_ratings + rate) / (self.count_of_ratings + 1)
         self.count_of_ratings += 1
