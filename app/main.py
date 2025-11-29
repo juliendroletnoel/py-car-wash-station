56b043128d611a7e49c0c71cad4d1555e67e611c
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-11-29 10:41:37 -0500

    Fixed average rating typo

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ class CarWashStation (object):
                  average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
-        self.avergage_rating = average_rating
+        self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list) -> float:
@@ -29,7 +29,7 @@ class CarWashStation (object):
     def calculate_washing_price(self, car: Car) -> float:
         price = car.comfort_class \
             * (self.clean_power - car.clean_mark) \
-            * (self.avergage_rating / self.distance_from_city_center)
+            * (self.average_rating / self.distance_from_city_center)
 
         return round(price, 1)
 
@@ -37,6 +37,6 @@ class CarWashStation (object):
         car.clean_mark = self.clean_power
 
     def rate_service(self, rate: float) -> None:
-        total_rates = (self.avergage_rating * self.count_of_ratings + rate)
-        self.avergage_rating = total_rates / (self.count_of_ratings + 1)
+        total_rates = (self.average_rating * self.count_of_ratings + rate)
+        self.average_rating = total_rates / (self.count_of_ratings + 1)
         self.count_of_ratings += 1
