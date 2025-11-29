7bc0e8bba0891fb464edc9ce4bd1cb7937d036a0
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-11-29 10:37:42 -0500

    Removed trailing spaces

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 class Car (object):
     # write your code here
-    def __init__(self, comfort_class: int, clean_mark: int, 
+    def __init__(self, comfort_class: int, clean_mark: int,
                  brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
@@ -9,7 +9,7 @@ class Car (object):
 
 class CarWashStation (object):
     # write your code here
-    def __init__(self, distance_from_city_center: float, clean_power: int, 
+    def __init__(self, distance_from_city_center: float, clean_power: int,
                  average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
@@ -37,6 +37,6 @@ class CarWashStation (object):
         car.clean_mark = self.clean_power
 
     def rate_service(self, rate: float) -> None:
-        self.avergage_rating = (self.avergage_rating \
-            * self.count_of_ratings + rate) / (self.count_of_ratings + 1)
+        total_rates = (self.avergage_rating * self.count_of_ratings + rate)
+        self.avergage_rating =  total_rates / (self.count_of_ratings + 1)
         self.count_of_ratings += 1
